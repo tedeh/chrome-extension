d240de4421ff3f27113b278a71c131bfccdf845d
Tedde Lundgren <mail@tedeh.net>
2022-08-31 12:47:19 +0700

    add initial files

---
+++ flashback.js
@@ -0,0 +1,6 @@
+const leaveAnchors = Array.from(document.querySelectorAll('a[href*="leave.php"]'));
+leaveAnchors.forEach(function (anchor) {
+  const url = new URL(anchor.href);
+  const realTargetUrl = url.searchParams.get('u');
+  anchor.href = realTargetUrl;
+});
---
+++ reddit.js
@@ -0,0 +1,5 @@
+const url = new URL(window.location);
+if (url.host.startsWith('www.reddit.com')) {
+  url.host = 'old.reddit.com';
+  window.location.replace(url.toString());
+}
