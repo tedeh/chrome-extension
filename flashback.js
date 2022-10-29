5f96cb752d9fbc3e3ecf7c4d12945b46bafae4ce
Tedde Lundgren <mail@tedeh.net>
2022-10-29 21:38:13 +0700

    fix flashback amp decoding

--- flashback.js
+++ flashback.js
@@ -2,7 +2,7 @@
   const leaveAnchors = Array.from(document.querySelectorAll('a[href*="leave.php"]'));
   leaveAnchors.forEach(function (anchor) {
     const url = new URL(anchor.href);
-    const realTargetUrl = url.searchParams.get('u');
+    const realTargetUrl = (url.searchParams.get('u') || '').replace(/&amp;/g, '&');
     anchor.href = realTargetUrl;
   });
 })();
